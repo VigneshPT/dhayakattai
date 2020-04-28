b4b271d62d890fbe666c760d72f1af227f9dcb89
Vignesh P T <vignesh294@gmail.com>
2020-04-28 12:10:36 +0530

    Added rolling dice feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const _ = require('lodash');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rolling Dhayakattai Soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const possibilities = [1,2,3,4,5,6,12];
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      currentDice: 0,
+      rolling: false
+    }
+  }
+  rollDhayakattai = () => {
+    return _.sample(possibilities);
+  }
+  handleClickRoll = () => {
+    this.setState({
+      rolling:true
+    })
+    setTimeout(()=> {
+      this.setState({
+        currentDice: this.rollDhayakattai(),
+        rolling: false
+      })
+    },500);
+  }
+
+  render() {
+    const {rolling, currentDice} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            rolling ? (
+              <p className="current-number">Rolling..</p>
+            ) : (
+              <p className="current-number">{currentDice}</p>
+            )
+          }
+          
+          <div>
+            <button className="roll-button" onClick={this.handleClickRoll}>Roll Dhayakattai</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
